450aa608cd62bc805cec747de5c394cc3e92b821
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2019-02-21 19:40:12 +0900

    commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const $iframe = document.querySelector('.modal .youtube-container iframe');
+const videoID = "LPjblqE3xHk"
+const $youtubeThumbs = document.querySelector('.youtube-thumbs');
+const $overlay = document.querySelector('.overlay');
+const $closeBtn = document.querySelector('.close');
+
+
+$youtubeThumbs.addEventListener("click", () => {
+  toggleModal();
+}, false);
+
+$overlay.addEventListener("click", () => {
+  toggleModal();
+}, false);
+
+$closeBtn.addEventListener("click", (event)=> {
+  event.preventDefault();
+  event.stopPropagation();
+  toggleModal();
+}, false);
+
+const toggleModal = () =>{
+  $overlay.classList.contains("is-show") ?
+    hideModal():
+    showModal()
+}
+
+const showModal = () =>{
+  $overlay.classList.add("is-show")
+  console.log($iframe);
+  $iframe.src=`${"https://www.youtube.com/embed/" + videoID}`;
+}
+const hideModal = () =>{
+  $overlay.classList.remove("is-show")
+  $iframe.src="";
+}
