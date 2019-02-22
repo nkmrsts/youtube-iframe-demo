c02f9069e139ac35ea9b84e03149ff9cf6a845f2
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2019-02-22 11:14:26 +0900

    IFrame Player APIをつかう

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-
-const $iframe = document.querySelector('.modal .youtube-container iframe');
-const videoID = "LPjblqE3xHk"
+const videoID = "mHrjM6oVez0"
 const $youtubeThumbs = document.querySelector('.youtube-thumbs');
 const $overlay = document.querySelector('.overlay');
 const $closeBtn = document.querySelector('.close');
 
+// IFrame Player API の読み込み
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-$youtubeThumbs.addEventListener("click", () => {
-  toggleModal();
-}, false);
+// YouTubeの埋め込み
+function onYouTubeIframeAPIReady() {
+  const ytPlayer = new YT.Player(
+    'target', { 
+      width: 640,
+      height: 390,
+      videoId: videoID,
+      playerVars: {
+        playsinline: 1
+      }
+    }
+  );
+  $youtubeThumbs.addEventListener("click", (event) => {
+    if (window.matchMedia( "(min-width: 768px)" ).matches) {
+      toggleModal();
+    } else {
+      event.target.classList.add('is-hide')
+      ytPlayer.playVideo();
+    }
+  }, false);
 
-$overlay.addEventListener("click", () => {
-  toggleModal();
-}, false);
+  $overlay.addEventListener("click", () => {
+    toggleModal();
+  }, false);
 
-$closeBtn.addEventListener("click", (event)=> {
-  event.preventDefault();
-  event.stopPropagation();
-  toggleModal();
-}, false);
+  $closeBtn.addEventListener("click", (event)=> {
+    event.preventDefault();
+    event.stopPropagation();
+    toggleModal();
+  }, false);
 
-const toggleModal = () =>{
-  $overlay.classList.contains("is-show") ?
-    hideModal():
-    showModal()
-}
-
-const showModal = () =>{
-  $overlay.classList.add("is-show")
-  $iframe.src=`${"https://www.youtube.com/embed/" + videoID }`;
-}
-const hideModal = () =>{
-  $overlay.classList.remove("is-show")
-  $iframe.src="";
+  let modalYtPlayer = new YT.Player(
+    'modalTarget', { 
+      width: 640,
+      height: 390,
+      videoId: videoID
+    }
+  );
+  const toggleModal = () => {
+    $overlay.classList.contains("is-show") ?
+      hideModal(ytPlayer):
+      showModal(ytPlayer)
+  }
+  const showModal = () =>{
+    $overlay.classList.add("is-show")
+  }
+  const hideModal = () =>{
+    modalYtPlayer.stopVideo();
+    $overlay.classList.remove("is-show")
+  }
 }
